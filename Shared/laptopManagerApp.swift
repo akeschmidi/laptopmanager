58fc985655b0d1c3050aa9aacbb7c7765851a841
Stefan Schwinghammer <ake_schmidi@me.com>
2021-10-27 20:29:18 +0200

    Initial Commit

---
+++ Shared/laptopManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  laptopManagerApp.swift
+//  Shared
+//
+//  Created by Stefan Schwinghammer on 27.10.21.
+//
+
+import SwiftUI
+
+@main
+struct laptopManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
