57c76e962bd9a690561bd61f40c40e7088a6ff53
Stefan Schwinghammer <ake_schmidi@me.com>
2022-01-07 18:22:46 +0100

    add first / Lastname add fach add teacher

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,24 +12,31 @@ import Foundation
 
 struct reservationView: View {
     
-    @AppStorage("name") var name: String = ""
-    @AppStorage("fach") var fach: String = ""
-    @AppStorage("laptopID") var laptopID1 = ""
-    @State  var date = Date()
-    @State private var selectedLaptopID = ""
+    @AppStorage("Vorname") var fistname: String = ""
+    @AppStorage("Nachname") var lastname: String = ""
     
+    @State private var selectedLaptopID = ""
+    @State private var selectedFach = ""
+    @State private var selectedTeacher = ""
+    
+    
+    @State  var date = Date()
     
     //Count how many laptops you need for this view
     var laptopIDList = ["1", "2", "3", "4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40"]
     
+    var teacherList = ["Stefan", "Diveres"]
     
-    func addData(name:String, fach:String, laptopID:String){
+    var fachList = ["Informatik", "Deutsch", "Diverses"]
+    
+    
+    func addData(fistname:String, lastname:String, fach:String, laptopID:String, techaer:String){
         
         //Init Firebase/Firestor
         let db = Firestore.firestore()
         
         
-        db.collection("reservation").addDocument(data: ["name": name, "fach": fach, "LaptopID": selectedLaptopID])
+        db.collection("reservation").addDocument(data: ["Firstname": fistname, "Lastname": lastname, "fach": selectedFach, "LaptopID": selectedLaptopID, "teacher": selectedTeacher])
     
     }
     
@@ -41,8 +48,19 @@ struct reservationView: View {
     //View beginns
     var body: some View {
         List {
-            TextField("Name", text: $name)
-            TextField("Fach", text: $fach)
+            TextField("Name", text: $fistname)
+            TextField("Nachname", text: $lastname)
+            
+            Picker("Fach", selection: $selectedFach) {
+                    ForEach(fachList, id: \.self) {
+                        Text($0)
+                        }
+                    }
+            Picker("Lehrperson", selection: $selectedTeacher ) {
+                    ForEach(teacherList, id: \.self) {
+                        Text($0)
+                        }
+                    }
             Picker("Laptopnummer", selection: $selectedLaptopID) {
                     ForEach(laptopIDList, id: \.self) {
                         Text($0)
@@ -55,17 +73,16 @@ struct reservationView: View {
             
             //Save Button
             Button(action: {
-                name = String(name)
-                fach = String(fach)
-                laptopID1 = String(laptopID1)
                 
                 //Add Data to Firebase
-                addData(name: name, fach: fach, laptopID: selectedLaptopID)
+                addData(fistname: fistname,lastname: lastname, fach: selectedFach, laptopID: selectedLaptopID, techaer: selectedTeacher)
                 
                 //Clear InputFields
-                name = ""
-                fach = ""
+                fistname = ""
+                lastname = ""
+                selectedFach = ""
                 selectedLaptopID = ""
+                selectedTeacher = ""
                 
             })
                     
